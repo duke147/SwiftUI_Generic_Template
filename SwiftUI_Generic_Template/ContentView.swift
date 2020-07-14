028d7fc89dac4ebcb8301e9973cbcecb37320fdd
duke147 <85614628@qq.com>
2020-07-14 20:51:14 +0800

    Initial Commit

---
+++ SwiftUI_Generic_Template/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Generic_Template
+//
+//  Created by 王富贵 on 2020/7/14.
+//  Copyright © 2020 王富贵. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
